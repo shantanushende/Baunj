04476801c825f5c34a237fd66e3f7642da66efb4
shan <shantanushende6@gmail.com>
2025-09-03 18:14:24 -0700

    Initial Commit

---
+++ Baunj/BaunjApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaunjApp.swift
+//  Baunj
+//
+//  Created by Shantanu Shende on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BaunjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaunjTests/BaunjTests.swift
@@ -0,0 +1,17 @@
+//
+//  BaunjTests.swift
+//  BaunjTests
+//
+//  Created by Shantanu Shende on 9/3/25.
+//
+
+import Testing
+@testable import Baunj
+
+struct BaunjTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
